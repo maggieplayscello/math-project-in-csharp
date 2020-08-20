104158f62a2d216f7df822c80a8a2ba22883140b
Maggie Thompson <maggieplayscello@gmail.com>
2020-08-20 14:44:10 -0400

    added try/catch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace math_project_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWould you like to do a math problem? ");
+            Console.WriteLine("\nWould you like to do a math problem? Enter Y for Yes or any other key to quit.");
             var yesOrNo = Console.ReadLine();
             while (string.Equals("y", yesOrNo, StringComparison.CurrentCultureIgnoreCase)) {
                 Random random = new Random();
@@ -15,22 +15,29 @@ namespace math_project_csharp
                 int answer = a + b;
                 Console.WriteLine(a + " + " + b + " = ");
                 
-                int userAnswer = int.Parse(Console.ReadLine());                
-
-                if (answer == userAnswer) {
-                    Console.WriteLine("That's correct! Here's another:");
-                } else {
-                    while (answer != userAnswer) {
-                        Console.WriteLine("That is incorrect. Guess again:");
-                        int tryAgain = int.Parse(Console.ReadLine());     
-                        if (tryAgain == answer) {
-                            Console.WriteLine("That's correct! Here's another:");
-                            break;
+                try {
+                    int userAnswer = int.Parse(Console.ReadLine());  
+                    if (answer == userAnswer) {
+                        Console.WriteLine("That's correct! Here's another:");
+                    } else {
+                        while (answer != userAnswer) {
+                            Console.WriteLine("That is incorrect. Guess again:");
+                            int tryAgain = int.Parse(Console.ReadLine());     
+                            if (tryAgain == answer) {
+                                Console.WriteLine("That's correct! Here's another:");
+                                break;
+                            }
                         }
                     }
-                }
+                } catch (Exception) {
+                    Console.WriteLine("Sorry, that wasn't a number. Press Q to Quit or any other key to continue playing.");
+                    if  (string.Equals("q", Console.ReadLine(), StringComparison.CurrentCultureIgnoreCase)) {
+                        Console.WriteLine("Ok, goodbye!");
+                        break;
+                    } 
+                }                              
             }   
-            Console.Write("\nPress any key to exit...");
+            Console.Write("\nPress any key to close the program.");
             Console.ReadKey(true);            
         }
     }
